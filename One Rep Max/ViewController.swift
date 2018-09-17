58d1e9b5f74cccfac3b2b222980b1b2a72cdd4c1
Student <student@s519771s-macbook-air.local>
2018-09-17 17:04:36 -0500

    Initial Commit

---
+++ One Rep Max/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  One Rep Max
+//
+//  Created by Student on 9/17/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
